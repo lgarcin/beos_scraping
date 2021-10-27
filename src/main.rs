adf732d5e4c2fe670bd4a222c525e05217c24eb1
lgarcin <laurent.garcin@gmail.com>
2021-10-27 17:39:21 +0200

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,96 @@
 use alert::alert;
 use clipboard::{ClipboardContext, ClipboardProvider};
+use regex::{Error, Regex};
 use select::document::Document;
 use select::node::Node;
 use select::predicate::Class;
 use std::env;
 
-fn parse(node: Node) -> String {
-    let mut res = String::new();
+const EXO_SEPARATOR: &str = "-----";
+
+fn parse_node(node: &Node) -> String {
+    let mut parsed_node = String::new();
     match node.name() {
         Some("ol") => {
-            res += r"\begin{question}";
-            res += "\n";
+            parsed_node += r"\begin{question}";
+            parsed_node += "\n";
             for child in node.children() {
-                res += &parse(child);
+                parsed_node += &parse_node(&child);
             }
-            res += r"\end{question}";
-            res += "\n";
+            parsed_node += r"\end{question}";
+            parsed_node += "\n";
         }
         Some("ul") => {
-            res += r"\begin{itemize}";
-            res += "\n";
+            parsed_node += r"\begin{itemize}";
+            parsed_node += "\n";
             for child in node.children() {
-                res += &parse(child);
+                parsed_node += &parse_node(&child);
             }
-            res += r"\end{itemize}";
-            res += "\n";
+            parsed_node += r"\end{itemize}";
+            parsed_node += "\n";
         }
         Some("li") => {
-            res += r"\item";
-            res += "\n";
+            parsed_node += r"\item";
+            parsed_node += "\n";
             match node.as_text() {
-                Some(text) => res += text,
+                Some(text) => parsed_node += text,
                 _ => {}
             };
             for child in node.children() {
-                res += &parse(child);
+                parsed_node += &parse_node(&child);
             }
-            res += "\n";
+            parsed_node += "\n";
         }
         Some("br") => {
-            res += "\n";
+            parsed_node += "\n";
         }
-        Some("u") => {}
-        Some("div") => {
-            res += "\n";
-            match node.as_text() {
-                Some(text) => res += text,
-                _ => {}
-            };
-            for child in node.children() {
-                res += &parse(child);
+        Some("u") => {
+            if node.text().contains("Exercice") {
+                parsed_node += "\n";
+                parsed_node += EXO_SEPARATOR;
+                parsed_node += "\n";
             }
-            //res += "\n"
         }
-        _ => {
+        other => {
+            if other == Some("div") {
+                parsed_node += "\n"
+            }
             match node.as_text() {
-                Some(text) => res += text,
+                Some(text) => {
+                    if text.contains("Exercice") {
+                        parsed_node += "\n";
+                        parsed_node += EXO_SEPARATOR;
+                        parsed_node += "\n";
+                    } else {
+                        parsed_node += text
+                    }
+                }
                 _ => {}
             };
-            for child in node.children() {
-                res += &parse(child);
+            for child in node
+                .children()
+                .take_while(|n| !n.text().starts_with("Indication"))
+            {
+                parsed_node += &parse_node(&child);
             }
         }
     }
-    res
+    parsed_node
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: Vec<String> = env::args().collect();
+fn parse_document(document: &Document) -> String {
+    let mut parsed_document = String::new();
+    for node in document.find(Class("tex2jax")) {
+        parsed_document += &parse_node(&node);
+    }
 
-    let nb_exo = &args[1];
-    let url = "https://beos.prepas.org/?q=Epreuve%20Orale%20".to_owned() + nb_exo;
+    if !parsed_document.contains(EXO_SEPARATOR) {
+        parsed_document = EXO_SEPARATOR.to_owned() + &parsed_document;
+    }
+    parsed_document
+}
 
-    let resp = reqwest::blocking::get(&url)?;
-    let document = Document::from_read(resp)?;
-
+fn preambule(document: &Document) -> String {
     let mut info = document
         .find(Class("field-label"))
         .into_selection()
@@ -85,20 +101,51 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .collect::<Vec<_>>();
     info.reverse();
 
-    let mut exo = String::new();
-    exo += r"\begin{exo}[comment=";
-    exo += &info.join(" ");
-    exo += "]\n";
+    let mut preambule = String::new();
+    preambule += r"\begin{exo}[comment=";
+    preambule += &info.join(" ");
+    preambule += "]\n";
+    preambule
+}
 
-    for node in document.find(Class("tex2jax")) {
-        exo += &parse(node);
-    }
+fn process_exo(exercice: &str) -> Result<String, Error> {
+    let mut exo = exercice.to_owned();
+    exo = Regex::new(r"(?s)(1\..*)")?
+        .replace_all(&exo, "\\begin{question}\n$1\\end{question}\n$2")
+        .into_owned();
+    exo = Regex::new(r"(?s)(a\).*?)(\n\s*\d\.)")?
+        .replace_all(&exo, "\n\\begin{question}\n$1\\end{question}\n$2")
+        .into_owned();
+    exo = Regex::new(r"(?s)(\s+)\d\.")?
+        .replace_all(&exo, "$1\\item ")
+        .into_owned();
+    exo = Regex::new(r"(?s)(\n\s+)[a-z]\)")?
+        .replace_all(&exo, "$1\\item ")
+        .into_owned();
+    Ok(exo)
+}
 
-    exo += r"\end{exo}";
-    exo += "\n";
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    let nb_exo = &args[1];
+    let url = "https://beos.prepas.org/?q=Epreuve%20Orale%20".to_owned() + nb_exo;
+
+    let resp = reqwest::blocking::get(&url)?;
+    let document = Document::from_read(resp)?;
+    let preambule = preambule(&document);
+
+    let response = parse_document(&document)
+        .split(EXO_SEPARATOR)
+        .skip(1)
+        .map(process_exo)
+        .flat_map(|x| x)
+        .map(|exo| preambule.clone() + &exo + "\\end{exo}\n")
+        .collect();
+
     alert("Beos scraping", "Copied into clipboard");
     let mut cx: ClipboardContext = ClipboardProvider::new()?;
-    cx.set_contents(exo)?;
+    cx.set_contents(response)?;
 
     Ok(())
 }
